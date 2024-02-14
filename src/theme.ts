4bb476ce2605d4e37ee2e421f01ea86d627e8c2d
simuseokyun <sim31059999@gmail.com>
2024-02-14 15:45:34 +0900

    feat : setting

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components/dist/types';
+
+export const lightTheme: DefaultTheme = {
+    bgColor: 'white',
+    textColor: 'black',
+    accentColor: 'red',
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor: 'black',
+    textColor: 'white',
+    accentColor: 'blue',
+};
