611a9e5a4575a552560c1f98aaf60d2e75fbdc63
simuseokyun <sim31059999@gmail.com>
2024-02-18 16:08:29 +0900

    feat : import delete

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+
 import App from './App';
-import { darkTheme, lightTheme } from './theme';
-import { BrowserRouter } from 'react-router-dom';
+
 import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
