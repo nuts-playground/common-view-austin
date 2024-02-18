0c80d6e37fe191ab63476b9df906d283c9241e8f
simuseokyun <sim31059999@gmail.com>
2024-02-18 16:01:53 +0900

    feat : 색상 변경

--- src/theme.ts
+++ src/theme.ts
@@ -4,10 +4,14 @@ export const lightTheme: DefaultTheme = {
     bgColor: '  #eff3f7',
     textColor: 'black',
     accentColor: 'red',
+    footerBgColor: 'black',
+    footerTextColor: '#eff3f7',
 };
 
 export const darkTheme: DefaultTheme = {
-    bgColor: 'black',
+    bgColor: '#1a1a1a',
     textColor: 'white',
     accentColor: 'blue',
+    footerBgColor: 'black',
+    footerTextColor: 'black',
 };
